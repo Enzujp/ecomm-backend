ef7c9c1320e2912ad786f2e90cf1c93981d7d239
Johnpaul <johnpaulokache@gmail.com>
2024-01-07 20:17:13 +0100

    add order routes

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const checkAuth = require("../middleware/checkAuth");
+
+const orderController = require("../controllers/orderController");
+
+router.get("/", checkAuth, orderController.get_all_orders);
+router.post("/", checkAuth, orderController.create_new_order);
+router.get("/:orderId", checkAuth, orderController.get_order_by_id);
+router.delete("/:orderId", checkAuth, orderController.delete_order);
