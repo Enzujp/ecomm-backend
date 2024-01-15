0997f6837430c232e075355145c4d368e00e9e2b
Johnpaul <johnpaulokache@gmail.com>
2024-01-15 22:35:22 +0100

    fix typo in middleware

--- src/middleware/checkAdmin.js
+++ src/middleware/checkAdmin.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.header("Authorization")
+        const token = req.headers("Authorization")
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
         req.userData = decoded;
         if (req.userData.role!= "Admin") {
